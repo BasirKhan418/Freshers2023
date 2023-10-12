cc67758b1a3af93d96bb48cdafe1620e0fae1c22
Basir Khan <khanbasir5555@gmail.com>
2023-10-12 13:01:04 +0530

    search coupon fix

--- src/pages/Couponclaimsection.js
+++ src/pages/Couponclaimsection.js
@@ -122,18 +122,18 @@ else{
       <div>   
     <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
     <div className="relative flex justify-center">
-        <div className="absolute inset-y-0 left-12 flex items-center pl-3 pointer-events-none lg:left-36">
+        <div className="absolute inset-y-0 left-6 flex items-center pl-3 pointer-events-none lg:left-20">
             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search"  name='regd' value={regd} onChange={handleChanges} id="default-search" className="block w-[80vw] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for coupon enter regd no." required/>
-        <button onClick={handleSearch}  className="text-white absolute right-14 lg:right-44 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <input type="search"  name='regd' value={regd} onChange={handleChanges} id="default-search" className="block w-[90vw] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for coupon enter regd no." required/>
+        <button onClick={handleSearch}  className="text-white absolute right-8 lg:right-28 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{loading1?"Searching.....":"Search"}</button>
     </div>
 </div>
 <div>
     
-{order!=null?<div className="relative overflow-x-auto my-4 mx-8 rounded-lg">
+{order!=null?<div className="relative overflow-x-auto my-4 mx-4 rounded-lg">
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
