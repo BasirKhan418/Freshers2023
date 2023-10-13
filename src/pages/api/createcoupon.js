364824c5d15775a372a32f12cfd03fab2879eae3
Basir Khan <khanbasir5555@gmail.com>
2023-10-13 08:58:18 +0530

    cpn changes

--- src/pages/api/createcoupon.js
+++ src/pages/api/createcoupon.js
@@ -2,6 +2,7 @@ import connectDb from "../middleware/mongoose";
 import Coupon from "../../../models/Coupon";
 const handler = async (req, res) => {
 if(req.method==='POST'){
+    
 let a = await Coupon.findOne({regd:req.body.regd});
 if(a){
     res.status(200).json({success:false,message:"Coupon Already Created with this regd no."});
@@ -16,7 +17,7 @@ else{
         let cpn1 = new Coupon({
             regd:req.body.regd,
             cpn:cpn,
-            clm:"not claimed"
+            clm:"Claimed"
         })
         await cpn1.save();
         res.status(200).json({success:true,message:"Coupon Created successfully",data:cpn});
